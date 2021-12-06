b986f978171bb81dc693f817b3d533f1b35f836d
davidwashere <20852399+davidwashere@users.noreply.github.com>
2021-12-06 00:20:20 -0600

    update d6 tests

--- 2021/day06/day06_test.go
+++ 2021/day06/day06_test.go
@@ -8,35 +8,35 @@ import (
 func TestP1(t *testing.T) {
 	got := part1("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 5934
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP1_Actual(t *testing.T) {
 	got := part1("input.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 390923
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP2(t *testing.T) {
 	got := part2("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 26984457539
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP2_Actual(t *testing.T) {
 	got := part2("input.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 1749945484935
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
