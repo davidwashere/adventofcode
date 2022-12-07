61fb369796ee3d20c146faa80fac5a38dc35717d
davidwashere <davidwashere@users.noreply.github.com>
2022-12-06 20:15:33 -0600

    workflow diff test

--- .github/diff.sh
+++ .github/diff.sh
@@ -5,11 +5,18 @@ dt=$(date)
 curdir=$(pwd)
 
 echo "Current Dir: $curdir"
+
+echo "list dir"
 ls -la
 
 echo "Making cache dir if not exist"
-mkdir -p ./.cache
+mkdir -p .cache
+
+echo "list dir"
+ls -la 
 
 echo "Appending to diff.out"
-echo ${dt} >> ./.cache/diff.out
+echo ${dt} >> .cache/diff.out
 
+echo "list cache"
+ls -la .cache
