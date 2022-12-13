b15c99c80855fea3cfdeae0f82958ab069d7e132
davidwashere <davidwashere@users.noreply.github.com>
2022-12-12 22:56:26 -0600

    state

--- tools/leader/leader.go
+++ tools/leader/leader.go
@@ -171,7 +171,7 @@ func publishToDiscord(results *strings.Builder) {
 		data := struct {
 			Content string `json:"content"`
 		}{
-			"```\nUpdates:\n\n" + results.String() + "\n```",
+			"```\nNew Stars Found:\n---\n\n" + results.String() + "\n```",
 		}
 
 		dataB, err := json.Marshal(data)
