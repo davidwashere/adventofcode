36583989edc52f903325ac593a88d32664c17149
David <dcaravello@gmail.com>
2022-12-17 22:35:56 -0600

    fix scaf line ending

---
+++ 2022/day16/day16.go
@@ -0,0 +1,30 @@
+package day16
+
+import (
+	"aoc/util"
+	"fmt"
+)
+
+func part1(inputfile string) int {
+	data, _ := util.ReadFileToStringSlice(inputfile)
+	// data, _ := util.ReadFileToIntSlice(inputfile)
+	// data, _ := util.ReadFileToStringSliceWithDelim(inputfile, "\n\n")
+	// grid := util.NewInfinityGridFromFile(inputfile, ".")
+
+	for _, line := range data {
+		tokens := util.ParseTokens(line)
+		// ints := util.ParseInts(line)
+		// strs := util.ParseStrs(line)
+		// words := util.ParseWords(line)
+
+		fmt.Println(tokens)
+	}
+
+	result := 0
+
+	return result
+}
+
+func part2(inputfile string) int {
+	return 0
+}
