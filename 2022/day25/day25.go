f275dda1cf9ee1127b8ae2a62b77b83ac19c5bcc
davidwashere <davidwashere@users.noreply.github.com>
2022-12-26 22:23:13 -0600

    day25

---
+++ 2022/day25/day25.go
@@ -0,0 +1,117 @@
+package day25
+
+import (
+	"aoc/util"
+	"fmt"
+	"math"
+)
+
+var (
+	digitMap = map[byte]int{
+		'2': 2,
+		'1': 1,
+		'0': 0,
+		'-': -1,
+		'=': -2,
+	}
+
+	snafMap = map[int]string{
+		0: "0",
+		1: "1",
+		2: "2",
+		3: "=",
+		4: "-",
+	}
+)
+
+func snafuToDec(snafu string) int {
+	size := len(snafu)
+	// l = 5
+	// i = 0 > 4
+	// i = 1 > 3
+	// i = 2 > 2
+	// i = 3 > 1
+	// i = 4 > 0
+
+	total := 0
+	for i := 0; i < len(snafu); i++ {
+		exp := (size - 1 - i)
+		multi := int(math.Pow(float64(5), float64(exp)))
+		dig := digitMap[snafu[i]]
+		total += multi * dig
+	}
+
+	return total
+}
+
+func decToSnafu(dec int) string {
+
+	carry := false
+	snafu := ""
+	for dec >= 0 {
+		quo := dec / 5
+		rem := dec % 5
+
+		if carry {
+			rem++
+
+			if rem == 5 {
+				quo++
+				rem = 0
+			}
+		}
+
+		dig := snafMap[rem]
+		snafu = fmt.Sprintf("%v%v", dig, snafu)
+		dec = quo
+
+		if rem >= 3 {
+			carry = true
+		} else {
+			carry = false
+		}
+
+		if dec == 0 && !carry {
+			break
+		}
+	}
+
+	// 0 = 0
+	// 1 = 1
+	// 2 = 2
+	// 3 = 1=
+	// 4 = 1-
+	// 5 = 10
+	// 6 = 11
+	// 7 = 12
+	// 8 = 2=
+	// 9 = 2-
+	// 10 = 20
+	// 11 = 21
+	// 12 = 22
+	// 13 = 1==
+	// 14 = 1=-
+	// 15 = 1=0
+	// 16 = 1=1
+	// 17 = 1=2
+	// 18 = 1-=
+	// 19 = 1--
+	// 20 = 110
+	return snafu
+}
+
+func part1(inputFile string) string {
+	data, _ := util.ReadFileToStringSlice(inputFile)
+	sum := 0
+	for _, line := range data {
+		sum += snafuToDec(line)
+	}
+
+	return decToSnafu(sum)
+}
+
+func part2(inputFile string) string {
+	// load(inputFile)
+
+	return ""
+}
---
+++ 2022/day25/day25_test.go
@@ -0,0 +1,104 @@
+package day25
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSnafuToDec(t *testing.T) {
+	tt := []struct {
+		in   string
+		want int
+	}{
+		{"1=-0-2", 1747},
+		{"12111", 906},
+		{"2=0=", 198},
+		{"21", 11},
+		{"2=01", 201},
+		{"111", 31},
+		{"20012", 1257},
+		{"112", 32},
+		{"1=-1=", 353},
+		{"1-12", 107},
+		{"12", 7},
+		{"1=", 3},
+		{"122", 37},
+	}
+
+	for _, tc := range tt {
+		got := snafuToDec(tc.in)
+		if got != tc.want {
+			t.Errorf("got %v want %v", got, tc.want)
+		}
+	}
+}
+
+func TestDecToSnafu(t *testing.T) {
+	tt := []struct {
+		in   int
+		want string
+	}{
+		{0, "0"},
+		{1, "1"},
+		{2, "2"},
+		{3, "1="},
+		{4, "1-"},
+		{5, "10"},
+		{1747, "1=-0-2"},
+		{906, "12111"},
+		{198, "2=0="},
+		{11, "21"},
+		{201, "2=01"},
+		{31, "111"},
+		{1257, "20012"},
+		{32, "112"},
+		{353, "1=-1="},
+		{107, "1-12"},
+		{7, "12"},
+		{3, "1="},
+		{37, "122"},
+	}
+
+	for _, tc := range tt {
+		got := decToSnafu(tc.in)
+		if got != tc.want {
+			t.Errorf("got %v want %v for in %v", got, tc.want, tc.in)
+		}
+	}
+}
+
+func TestP1(t *testing.T) {
+	got := part1("sample.txt")
+	fmt.Printf("Got: %v\n", got)
+	want := "2=-1=0"
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
+}
+
+func TestP1_Actual(t *testing.T) {
+	got := part1("input.txt")
+	fmt.Printf("Got: %v\n", got)
+	want := ""
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
+}
+
+func TestP2(t *testing.T) {
+	got := part2("sample.txt")
+	fmt.Printf("Got: %v\n", got)
+	want := ""
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
+}
+
+func TestP2_Actual(t *testing.T) {
+	got := part2("input.txt")
+	fmt.Printf("Got: %v\n", got)
+	want := ""
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
+}
