9faec4b50e292b6db9455e9081767b9c56c7b25a
Dave <20852399+davidwashere@users.noreply.github.com>
2025-12-03 00:02:48 -0600

    2025 d1

--- 2025/day01/day01.go
+++ 2025/day01/day01.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 )
 
-var (
+type singleStep struct {
+	dir  string
+	dist int
+}
 
+var (
+	steps = []singleStep{}
 )
 
 func load(inputFile string) {
@@ -20,19 +25,81 @@ func load(inputFile string) {
 		// ints := util.ParseInts(line)
 		// strs := util.ParseStrs(line)
 		// words := util.ParseWords(line)
+		step := singleStep{
+			dir:  string(line[0]),
+			dist: tokens.Ints[0],
+		}
 
-		fmt.Println(tokens)
+		steps = append(steps, step)
+
+		// fmt.Println(tokens)
 	}
+	fmt.Println(steps)
 }
 
 func part1(inputFile string) int {
 	load(inputFile)
 
-	return 0
+	max := 99
+	pos := 50
+	count := 0
+	for i, step := range steps {
+		if step.dir == "L" {
+			pos = (pos - step.dist) % (max + 1)
+			if pos < 0 {
+				pos = (max + 1) + pos
+			}
+		} else {
+			pos = (pos + step.dist) % (max + 1)
+		}
+
+		fmt.Printf("[%d] %d\n", i, pos)
+		if pos == 0 {
+			count++
+		}
+	}
+
+	return count
 }
 
 func part2(inputFile string) int {
 	load(inputFile)
 
-	return 0
+	max := 100
+	pos := 50
+	count := 0
+	for _, step := range steps {
+		start := pos
+		// every 100 dist = guaranteed 1 click
+		if step.dist >= max {
+			count += step.dist / int(100)
+		}
+		step.dist = step.dist % max
+
+		if step.dir == "L" {
+			pos = pos - step.dist
+
+			if pos == 0 {
+				count++
+			} else if pos < 0 {
+				if start != 0 {
+					count++
+				}
+
+				pos = max + pos // wraps around (pos is -, so this is effectively max - pos)
+			}
+		} else {
+			pos = pos + step.dist
+
+			if pos > 99 {
+				count++
+				pos = pos - max
+			}
+		}
+
+		fmt.Printf("[%s%d] %d - clicks: %d\n", step.dir, step.dist, pos, count)
+	}
+
+	return count
+
 }
--- 2025/day01/day01_test.go
+++ 2025/day01/day01_test.go
@@ -8,7 +8,7 @@ import (
 func TestP1(t *testing.T) {
 	got := part1("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	want := 0
+	want := 3
 	if got != want {
 		t.Errorf("got %v but want %v", got, want)
 	}
@@ -17,7 +17,7 @@ func TestP1(t *testing.T) {
 func TestP1_Actual(t *testing.T) {
 	got := part1("input.txt")
 	fmt.Printf("Got: %v\n", got)
-	want := 0
+	want := 1120
 	if got != want {
 		t.Errorf("got %v but want %v", got, want)
 	}
@@ -26,7 +26,7 @@ func TestP1_Actual(t *testing.T) {
 func TestP2(t *testing.T) {
 	got := part2("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	want := 0
+	want := 6
 	if got != want {
 		t.Errorf("got %v but want %v", got, want)
 	}
@@ -35,7 +35,7 @@ func TestP2(t *testing.T) {
 func TestP2_Actual(t *testing.T) {
 	got := part2("input.txt")
 	fmt.Printf("Got: %v\n", got)
-	want := 0
+	want := 6554
 	if got != want {
 		t.Errorf("got %v but want %v", got, want)
 	}
